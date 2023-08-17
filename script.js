90f9007c21b6eabdf3da698425f9aa561cc41311
kapoorgautam <20b1039.bca1@gmail.com>
2023-08-17 12:01:48 +0530

    responsive

--- script.js
+++ script.js
@@ -6,28 +6,9 @@ tl.from("#nav img , #nav h2  , #nav button", {
      opacity: 0,
      stagger:0.1,
 })
-tl.from("#main h1",{
-    duration: 0.5,
-    y: 150,
-    opacity: 0,
-    // stagger:0.1,
-})
-tl.from("#main #bg-img",{
-    duration: 0.5,
-    x: -130,
-    opacity: 0,
-    scale: 1,
-})
-tl.from("#main .btn button",{
+tl.from("#hero .btn button",{
     x: -150,
     opacity: 0,
 })
-tl.from("#main .right ul",{
-    x: -100,
-    opacity: 0,
-})
-tl.from("#main .left ul",{
-    x: 100,
-    opacity: 0,
-})
+
 
